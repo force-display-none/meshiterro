478b4234a519323b95dd8d0868f68e547404cadd
kckw0917 <kckw0917@gmail.com>
2018-06-18 20:35:29 +0900

    Start5-6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,9 @@ class UsersController < ApplicationController
     # 下記のように記述することで@post_imagesにはそのページのユーザー(@user)に紐づいた投稿(.post_images)のみを渡すことができるので全体の投稿ではなく、個人が投稿したもののみを表示することができます
   	  @post_images = @user.post_images.page(params[:page])
   end
+
+  def edit
+  	# ここもユーザー1件のデータを取得する記述です　編集用のビューにはform_forを使用しますのでユーザーの情報を一件取得する記述をしましょう。 params[:id]からユーザーの情報を取得し、インスタンス(@user)にしまいます
+      @user = User.find(params[:id])
+  end
 end
